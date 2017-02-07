13223b90e59a3732c57ef84a777f5a40aa39af17
Cristina <cblanf01@estudiantes.unileon.es>
2017-02-08 00:26:58 +0100

    librerias añadidas y configuracion del proyecto

--- src/unileon/tfm/cbf/Main.java
+++ src/unileon/tfm/cbf/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("HolaMundo");
+        int manzana = 3;
+        int pera = 2;
+        System.out.println(manzana + pera);
+
     }
 }
