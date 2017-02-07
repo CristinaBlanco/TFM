9828960d882e5bda3149efdbf4d4e512eb770766
Cristina <cblanf01@estudiantes.unileon.es>
2017-02-08 00:32:27 +0100

    Empezando con la base de datos

---
+++ src/unileon/tfm/cbf/unileon/tfm/cbf/database/Conexion.java
@@ -0,0 +1,10 @@
+package unileon.tfm.cbf.unileon.tfm.cbf.database;
+
+/**
+ * Created by Cristina Blanco Fernandez.
+ */
+public class Conexion {
+    public static void main(String args[]) {
+
+    }
+}
